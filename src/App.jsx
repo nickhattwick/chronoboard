b8d54a8e30a6feea6a354f3330386609d0c69106
Nick Hattwick <nhattwick@gmail.com>
2025-02-26 22:11:36 -0500

    Implement Projects Page
    Fixes #7
    
    ---
    
    For more details, open the [Copilot Workspace session](https://copilot-workspace.githubnext.com/nickhattwick/chronoboard/issues/7?shareId=XXXX-XXXX-XXXX-XXXX).

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import TaskBoard from './components/TaskBoard';
 import CalendarPage from './components/CalendarPage';
+import ProjectsPage from './components/ProjectsPage';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/board" element={<TaskBoard />} />
         <Route path="/calendar" element={<CalendarPage />} />
+        <Route path="/projects" element={<ProjectsPage />} />
         {/* Add other routes here */}
       </Routes>
     </div>
